0992ebc9a83a79b7957b4d9e922c34e6455889da
stephrhee <srhee93@gmail.com>
2015-12-01 19:57:28 -0500

    record transcript of moves and game score in a file called transcript

--- term2048/automate.py
+++ term2048/automate.py
@@ -6,6 +6,8 @@ from random import randint
 
 from game import Game
 
+transcript_file_name = "transcript"
+
 class Mode(Enum):
     RANDOM = 1
     LEARN = 2
@@ -15,6 +17,7 @@ def play_game(mode=Mode.RANDOM):
     Automates game playing by deciding the next move according to mode.
     """
     game = Game()
+    moves = []
 
     margins = {'left': 4, 'top': 4, 'bottom': 4}
 
@@ -30,6 +33,7 @@ def play_game(mode=Mode.RANDOM):
             
             if (mode == Mode.RANDOM):
                 m = get_random_move()
+                moves.append(str(m))
             else:  
                 sys.exit("unsupported learning mode")
 
@@ -42,6 +46,7 @@ def play_game(mode=Mode.RANDOM):
 
     game.saveBestScore()
     print('You won!' if game.board.won() else 'Game Over')
+    record_game(game, moves)
     return game.score
 
 def get_random_move():
@@ -52,3 +57,15 @@ def get_random_move():
     """
     random_move = randint(1, 4)
     return random_move
+
+def record_game(game, moves):
+    """
+    Appends the game transcript to the end of the file defined by transcript_file_name.
+    A transcript is string of all moves played in the game followed by the game score,
+    with each item separated by a " ".
+    """
+    transcript = " ".join(moves) + " " + str(game.score) + "\n"
+
+    transcript_file = open(transcript_file_name, 'a')
+    transcript_file.write(transcript)
+    transcript_file.close()
